4da17fd77c4d1a4b3d277f7ca165b6deacd7b6b4
Tu Nombre <tuemail@example.com>
2026-03-24 09:46:34 -0500

    feat: lista de clientes terminada

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cliente
 
-# Create your views here.
+def lista_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, "clientes/lista.html", {"clientes":clientes})
+
