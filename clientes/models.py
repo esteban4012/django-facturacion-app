02fe28a9e3c9c3cff63d9bdb94d760613e75c13d
Tu Nombre <tuemail@example.com>
2026-03-24 09:26:17 -0500

    feat: crear modelo de clientes

---
+++ clientes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+    telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
