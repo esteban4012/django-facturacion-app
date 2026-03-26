5093b2ea5a4534ffe889fbda6d9e59ba462c4be8
Tu Nombre <tuemail@example.com>
2026-03-26 08:06:49 -0500

    refactor: separar el dashboard del home y mejorar la estructura UI/UX

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 from .views import home
 from django.contrib.auth import views as auth_views
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +29,6 @@ urlpatterns = [
     path('facturas/', include('facturas.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 
 ]
--- config/views.py
+++ config/views.py
@@ -1,5 +1,32 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from facturas.models import Factura
+from clientes.models import Cliente
+from productos.models import Producto
+from django.db.models import Sum
+from datetime import date
 
+@login_required
+def dashboard(request):
+    total_facturado = Factura.objects.aggregate(Sum('total'))['total__sum'] or 0
+    total_facturas = Factura.objects.count()
+    total_clientes = Cliente.objects.count()
+
+    hoy = date.today()
+    ventas_hoy = Factura.objects.filter(fecha=hoy).aggregate(Sum('total'))['total__sum'] or 0
+
+    productos_bajo_stock = Producto.objects.filter(stock__lt=5)
+
+    context = {
+        'total_facturado': total_facturado,
+        'total_facturas': total_facturas,
+        'total_clientes': total_clientes,
+        'ventas_hoy': ventas_hoy,
+        'productos_bajo_stock': productos_bajo_stock
+    }
+
+    return render(request, 'dashboard.html', context)
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request,'home.html')
+
